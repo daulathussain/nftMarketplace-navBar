b24b7e1c130ed8f69d88e5ab4cba52cf26e15895
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2023-10-17 10:25:57 +0100

    Added connect wallet button to navbar to improve UX

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -124,6 +124,7 @@ const NavBar = () => {
           {/* CREATE BUTTON SECTION */}
           <div className={Style.navbar_container_right_button}>
             <Button btnName="Create" handleClick={() => {}} />
+            <Button btnName="Connect Wallet" handleClick={() => {}}/>
           </div>
 
           {/* USER PROFILE */}
